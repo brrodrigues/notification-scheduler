9f716935c6dcedf62593a2badee8dc285ca06167
Bruno Rodrigues <brrodrigues@live.com>
2018-01-24 16:54:54 -0200

    Ajuste para exibir a id especifico da classe Recipient, Bot, User
    Signed-off-by: Bruno Rodrigues <brrodrigues@live.com>

---
+++ src/main/java/br/com/lasa/notificacao/config/ExposeEntityIdRestConfiguration.java
@@ -0,0 +1,20 @@
+package br.com.lasa.notificacao.config;
+
+import br.com.lasa.notificacao.domain.lais.BotUser;
+import br.com.lasa.notificacao.domain.lais.Conversation;
+import br.com.lasa.notificacao.domain.lais.Recipient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class ExposeEntityIdRestConfiguration extends RepositoryRestConfigurerAdapter {
+
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Recipient.class, BotUser.class, Conversation.class);
+    }
+
+
+}
