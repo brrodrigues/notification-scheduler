044fb2043b91de16037ccebb86b42b36ea45b2a9
Bruno Rodrigues <brrodrigues@live.com>
2018-02-07 15:02:39 -0200

    Customizacao do endpoint /Lojas no metodo PATCH para ajustar o horarios para todas os dias.
    Signed-off-by: Bruno Rodrigues <brrodrigues@live.com>

---
+++ src/main/java/br/com/lasa/notificacao/rest/assembler/LojaResourcesAssembler.java
@@ -0,0 +1,30 @@
+package br.com.lasa.notificacao.rest.assembler;
+
+import br.com.lasa.notificacao.domain.Loja;
+import br.com.lasa.notificacao.repository.LojaRepository;
+import br.com.lasa.notificacao.rest.resource.LojaResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class LojaResourcesAssembler extends ResourceAssemblerSupport<Loja, LojaResource> {
+
+    @Autowired
+    RepositoryEntityLinks repositoryEntityLinks;
+
+    public LojaResourcesAssembler() {
+        super(LojaRepository.class, LojaResource.class);
+    }
+
+    @Override
+    public LojaResource toResource(Loja loja) {
+        Link lojaLink = repositoryEntityLinks.linkToSingleResource(Loja.class, loja.getId());
+        Link selfLink = new Link(lojaLink.getHref(), Link.REL_SELF);
+        return new LojaResource(loja, Arrays.asList(selfLink, lojaLink));
+    }
+}
