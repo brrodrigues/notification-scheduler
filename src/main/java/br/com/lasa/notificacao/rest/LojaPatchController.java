46cad50df760f13912105926756dba969c1bc798
Bruno Rodrigues <brrodrigues@live.com>
2018-02-07 16:05:52 -0200

    Customizacao do endpoint /Lojas no metodo PATCH para ajustar o horarios para todas os dias.
    Signed-off-by: Bruno Rodrigues <brrodrigues@live.com>

--- src/main/java/br/com/lasa/notificacao/rest/LojaPatchController.java
+++ src/main/java/br/com/lasa/notificacao/rest/LojaPatchController.java
@@ -4,8 +4,6 @@ import br.com.lasa.notificacao.domain.Loja;
 import br.com.lasa.notificacao.service.LojaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
-import org.springframework.data.rest.webmvc.RepositorySearchesResource;
-import org.springframework.hateoas.ResourceProcessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,23 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @BasePathAwareController
-@RequestMapping( value = "/lojas")
-public class LojaPatchController  implements ResourceProcessor<RepositorySearchesResource> {
+public class LojaPatchController  {
 
     @Autowired
     private LojaService lojaService;
 
-    @RequestMapping( method= RequestMethod.PATCH)
+    @RequestMapping( value = "lojas", method= RequestMethod.PATCH, produces= "application/hal+json")
     public ResponseEntity<Loja> patch(@RequestBody Loja loja) {
 
         Loja atualizar = lojaService.atualizar(loja);
 
-        return new ResponseEntity<Loja>(atualizar, HttpStatus.OK);
+        return new ResponseEntity(atualizar, HttpStatus.OK);
     }
 
-    @Override
-    public RepositorySearchesResource process(RepositorySearchesResource repositorySearchesResource) {
-        return repositorySearchesResource;
-    }
 
 }
