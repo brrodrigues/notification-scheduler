2b7f5615c1dfe23c21011bed9e12df3cd4fcf7b3
Bruno Rodrigues <brrodrigues@live.com>
2018-03-29 14:41:28 -0300

    Teste de envio de email
    Signed-off-by: Bruno Rodrigues <brrodrigues@live.com>

--- src/main/java/br/com/lasa/notificacao/config/mongo/MongoConfig.java
+++ src/main/java/br/com/lasa/notificacao/config/mongo/MongoConfig.java
@@ -1,8 +1,13 @@
 package br.com.lasa.notificacao.config.mongo;
 
+import br.com.lasa.notificacao.config.mongo.convert.AccessGroupConvert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.convert.CustomConversions;
+
+import java.util.Arrays;
 
 @Configuration
 public class MongoConfig {
@@ -10,10 +15,10 @@ public class MongoConfig {
     @Autowired
     private ApplicationContext context;
 
- /*  @Bean
+   @Bean
     CustomConversions customConversions() {
         AccessGroupConvert accessGroupConvert = context.getBean(AccessGroupConvert.class);
         return new CustomConversions(Arrays.asList(accessGroupConvert));
-    }*/
+    }
 
 }
