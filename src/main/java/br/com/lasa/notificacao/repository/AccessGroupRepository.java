dd40a7f810317de22f9868d532a5cafbade38f3a
edmsilva <brrodrigues@live.com>
2018-04-09 14:10:05 -0300

    Adicionando crossdomain
    Signed-off-by: edmsilva <brrodrigues@live.com>

--- src/main/java/br/com/lasa/notificacao/repository/AccessGroupRepository.java
+++ src/main/java/br/com/lasa/notificacao/repository/AccessGroupRepository.java
@@ -3,7 +3,9 @@ package br.com.lasa.notificacao.repository;
 import br.com.lasa.notificacao.domain.document.AccessGroup;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(value = "*")
 @RepositoryRestResource( path = "/access-groups")
 public interface AccessGroupRepository extends MongoRepository<AccessGroup, String> {
 
--- src/main/java/br/com/lasa/notificacao/repository/PermissionRepository.java
+++ src/main/java/br/com/lasa/notificacao/repository/PermissionRepository.java
@@ -3,7 +3,9 @@ package br.com.lasa.notificacao.repository;
 import br.com.lasa.notificacao.domain.document.Permission;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(value = "*")
 @RepositoryRestResource( path = "/permissions", collectionResourceRel = "permissions", itemResourceRel = "permissions")
 public interface PermissionRepository extends MongoRepository<Permission, String> {
 
