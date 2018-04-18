6e363053ee8564f611d9529217859bec1038fc78
Bruno Rodrigues <brrodrigues@live.com>
2018-04-18 18:53:07 -0300

    Configuração de envio de e-mail.
    Signed-off-by: Bruno Rodrigues <brrodrigues@live.com>

--- src/main/java/br/com/lasa/notificacao/rest/EmailController.java
+++ src/main/java/br/com/lasa/notificacao/rest/EmailController.java
@@ -5,17 +5,20 @@ import br.com.lasa.notificacao.service.Mailer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/email")
+@RequestMapping("/api/send-email")
 public class EmailController {
 
     @Autowired
     private Mailer mailer;
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity send(@RequestBody EnvioEmailRequest request){
+    public ResponseEntity send(@RequestBody EnvioEmailRequest request) {
         mailer.sendEmail(request.getPara(), request.getAssunto(),request.getMensagem());
         return ResponseEntity.ok("E-mail enviado com sucesso.");
     }
