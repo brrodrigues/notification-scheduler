2e252ebb960da40b697e4994b4bf1a7a675d74a1
Bruno Rodrigues <brrodrigues@live.com>
2018-04-20 17:20:52 -0300

    Mudando email do usuario para envio
    Signed-off-by: Bruno Rodrigues <brrodrigues@live.com>

--- src/main/java/br/com/lasa/notificacao/service/MailerImpl.java
+++ src/main/java/br/com/lasa/notificacao/service/MailerImpl.java
@@ -1,6 +1,7 @@
 package br.com.lasa.notificacao.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -12,11 +13,14 @@ public class MailerImpl implements Mailer {
 
     @Autowired private JavaMailSender javaMailSender;
 
+    @Value("${spring.mail.username}")
+    private String to;
+
     @Override
     public void sendEmail(String sendTo, String subject, String message) {
 
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom(sendTo);
+        simpleMailMessage.setFrom(to);
         simpleMailMessage.setTo(sendTo);
         simpleMailMessage.setSentDate(new Date());
         simpleMailMessage.setText(message);
